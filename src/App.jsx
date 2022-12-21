d52a3b70485fab9867ff6829315b9a766bde1ab5
Saleck-Eljili <salek.eljili@gmail.com>
2022-12-21 14:47:05 +0100

    feat: navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-import "./App.css";
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter as Router } from "react-router-dom";
 function App() {
-  return <div className="App">RICK-MORTY</div>;
+  return(
+    <Router> 
+      <div className="App">
+        <Navbar />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
