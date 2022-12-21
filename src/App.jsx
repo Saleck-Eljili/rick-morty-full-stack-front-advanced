a40dd80c92be590241a7a7ad997ad832a9afffa2
Saleck-Eljili <salek.eljili@gmail.com>
2022-12-21 14:54:36 +0100

    feat: react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
 import Navbar from "./components/Navbar/Navbar";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Episodes from "./Pages/Episodes";
+import Location from "./Pages/Location";
 function App() {
   return(
-    <Router> 
+    <Router>
       <div className="App">
         <Navbar />
       </div>
+      <Routes>
+
+        <Route path="/episodes" element={<Episodes />} />
+
+        <Route path="/location" element={<Location />} />
+      
+      </Routes>
     </Router>
   );
 }
